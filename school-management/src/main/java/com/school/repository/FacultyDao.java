d07b82c823ef8d877443596a133070ace82a7233
kajalyadav04 <kajalraj1520@gmail.com>
2025-06-26 20:29:44 +0530

    changes

--- school-management/src/main/java/com/school/repository/FacultyDao.java
+++
@@ -1,8 +0,0 @@
-package com.school.repository;
-
-import com.school.model.Faculty;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacultyDao extends JpaRepository<Faculty,Integer> {
-
-}
